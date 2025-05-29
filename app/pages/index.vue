ab0f7fe4eaf8d32c520875d6aee59cce54868de4
tcastanie <thibaut.castanie@gmail.com>
2025-05-29 14:40:50 +0200

    🎨 Add a little touch of uselessness

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -45,20 +45,25 @@ const links = ref([
         :ui="{ description: 'text-lg', links: 'flex-col' }"
       >
         <template #title>
-          <UUser
-            name="Thibaut Castanié"
-            :description="$t('main_subtitle')"
-            :avatar="{
-              src: '/tronche.jpg',
-              alt: 'Thibaut Castanié',
-              quality: 95,
-              width: 64,
-              height: 64,
-            }"
-            size="3xl"
-            :ui="{ name: 'font-hubot text-4xl', description: 'font-normal text-2xl tracking-normal', root: 'gap-4', avatar: 'size-16' }"
-            class="mb-16"
-          />
+          <div class="relative grid h-[240px] content-end overflow-hidden">
+            <UUser
+              name="Thibaut Castanié"
+              :description="$t('main_subtitle')"
+              :avatar="{
+                src: '/tronche.jpg',
+                alt: 'Thibaut Castanié',
+                quality: 95,
+                width: 64,
+                height: 64,
+              }"
+              size="3xl"
+              :ui="{ name: 'font-hubot text-4xl', description: 'font-normal text-2xl tracking-normal', root: 'gap-4', avatar: 'size-16' }"
+              class="mb-16"
+            />
+            <InteractiveGridPattern
+              class="[mask-image:radial-gradient(250px_circle_at_center,white,transparent)] inset-0 h-[200%] -skew-x-6 -skew-y-3"
+            />
+          </div>
         </template>
         <template #links>
           <div class="text-muted text-pretty text-lg">
